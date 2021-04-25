96fa75a8a42f5475f98c31648f9b5c3a593e9914
mirzahab85 <mirza.habul@telemach.ba>
2021-04-25 17:04:29 +0200

    Start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Movie from "./components/Movie";
+
+import React, { useEffect, useState } from "react";
+
+const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2ae13bacb05742feb24cddf8e6d72335&page=1";
+
+const IMG_API = "https://image.tmdb.org/t/p/w500";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=2ae13bacb05742feb24cddf8e6d72335&query="
+
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {   
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+       setMovies(data.results);
+    });
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+  if(searchTerm.length > 2) {
+        getMovies(SEARCH_API + searchTerm);
+
+        setSearchTerm("");
+    }
+  };
+
+  const handleOnChnage = (e) => {
+    setSearchTerm(e.target.value);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input 
+            className="search" 
+            type="search" 
+            placeholder="Search..."
+            value={searchTerm} 
+            onChange={handleOnChnage}
+        />
+        </form>
+    </header>
+      <div className="movie-container">
+        {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+      </>
   );
 }
 
+        
 export default App;
