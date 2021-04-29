417b1df650ee4e839495b51856f14a05dc29e90f
mirzahab85 <mirza.habul@telemach.ba>
2021-04-29 23:24:40 +0200

    Added details about the movie and made a back button

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 
-
 const setVoteClass = (vote) => {
     if (vote >= 8) {
         return "green";
@@ -13,12 +12,13 @@ const setVoteClass = (vote) => {
 };
 
 const IMG_API = "https://image.tmdb.org/t/p/w500";
-const Movie = ({title, poster_path, overview, vote_average}) => (
-    <div className="movie">
+const Movie = ({movie, title, details, poster_path, overview, vote_average, setDetailMovie}) => (
+    <div className="movie" onClick={() => setDetailMovie(movie)}>
         <img src={IMG_API + poster_path} alt={title} />
         <div className="movie-info">
         <h3>{title}</h3>
-             
+        
+     
         <span className={
             `tag ${setVoteClass(vote_average)}`
             }>
